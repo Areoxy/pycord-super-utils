54777c57b0fb5bb69eb12184f4ef21a2def30f08
Areo <86875820+Areoxy@users.noreply.github.com>
2022-01-07 08:56:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         "pycordSuperUtils.music.lavalink": ["*"],
     },
     include_package_data=True,
-    version="0.3.0",
+    version="0.3.1",
     license="MIT",
     description="A fork of discordSuperUtils to easily develop discord Bots with pycord",
     long_description=README,
