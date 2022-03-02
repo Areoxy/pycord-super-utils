bc9e5ff5de19fbd496e2ab9ece0d81368d69ed9c
Areo <86875820+Areoxy@users.noreply.github.com>
2022-03-02 17:51:00 +0100

    Updated example
    Added new example for paginator 
    
    Thanks to Heapy for making the example :)

--- examples/paginator.py
+++ examples/paginator.py
@@ -7,67 +7,59 @@ import pycordSuperUtils
 
 client = commands.Bot(command_prefix="-")
 
-embedc = discord.Color(0, 208, 255)
+embedc = discord.Color.from_rgb(255, 255, 255)
 
 
 @client.event
 async def on_ready():
-    print("Page manager is ready.", bot.user)
+    print("Page manager is ready.", client.user)
 
      
 
 # The old buttonpaginator was removed from PSU! Here's a example for the official py-cord paginator
     
-@client.command()
-async def paginator(ctx):
+@client.slash_command(name="paginator", description="Page manager")
+async def _paginator(ctx):
     await ctx.defer()
-    prefix = "/"
     invite = "https://discord.com/api/oauth2/authorize?client_id=828902907084537867&permissions=8&scope=bot%20applications.commands"
     page1 = Embed(
         color=embedc,
-        title="**🤖 Help Menu**",
-        timestamp=datetime.datetime.utcnow()
+        title="**🤖 Help Menu**"
     )
-    page1.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon.url)
-    page1.set_thumbnail(url=client.user.avatar.url)
-    page1.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar.url)
+    page1.set_footer(text=ctx.guild.name)
+    page1.set_author(name=ctx.author.display_name)
     page1.add_field(name="**🌐Features**", value="> Moderation \n> Information\n> Fun\n> Helpful Setups like applications, join2create and much more!", inline=False)
-    page1.add_field(name="**📊 Stats**", value=f"```py\nGuilds: {len(client.guilds)}\nUsers: {len(client.users)}\nUptime: 5 minutes```", inline=False)
+    page1.add_field(name="**📊 Stats**", value=f"```py\nGuilds: {len(client.guilds)}\nUsers: {len(client.users)}```", inline=False)
     page1.add_field(name="**🆘 Help & Links**", value=f"**Support Server**\n> [Join here](https://discord.gg/mQkydPS82f)\n**Developer**\n> Areo | 513786050271772673\n**Invite me**\n> [Invite now!]({invite})", inline=False)
 
     page2 = Embed(
         color=embedc,
         title="**🤖 Bot Help Menu**",
-        description=f"**🔨 Moderation Commands**",
-        timestamp=datetime.datetime.utcnow()
+        description=f"**🔨 Moderation Commands**"
     )
-    page2.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon.url)
-    page2.set_thumbnail(url=client.user.avatar.url)
+    page2.set_footer(text=ctx.guild.name)
 
     page3 = Embed(
         color=embedc,
         title="**🤖 Bot Help Menu**",
-        description=f"**ℹ Information Commands**",
-        timestamp=datetime.datetime.utcnow()
+        description=f"**ℹ️ Information Commands**"
     )
 
     page4 = Embed(
         color=embedc,
         title="**🤖 Bot Help Menu**",
-        description=f"**🎭 Fun Commands**",
-        timestamp=datetime.datetime.utcnow()
+        description=f"**🎭 Fun Commands**"
     )
 
     page5 = Embed(
         color=embedc,
         title="**🤖 Bot Help Menu**",
-        description=f"**⚙ Setup Commands**",
-        timestamp=datetime.datetime.utcnow()
+        description=f"**⚙️ Setup Commands**"
     )
-    page5.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon.url)
+    page5.set_footer(text=ctx.guild.name)
     page5.set_thumbnail(url=client.user.avatar.url)
     
-
+    # Note: You can add as much embed as you want
     paginationList = [page1, page2, page3, page4, page5]
 
     # Creating buttons
@@ -90,4 +82,4 @@ async def paginator(ctx):
     await paginator.respond(ctx.interaction, ephemeral=False)
 
 
-bot.run("token")
+client.run("token")
